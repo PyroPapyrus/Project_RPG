46c2e154f6ce9cac3dbea464bd1fde90b410e3b1
ManelPechim <emanuel.pechim@grupointegrado.br>
2025-05-28 17:01:46 -0300

    alterações basicas no close modal button

--- src/components/ui/close-modal-button.tsx
+++ src/components/ui/close-modal-button.tsx
@@ -1,8 +1,9 @@
 interface CloseModalButtonProps {
   onClose: () => void;
+  className?: string;
 }
 
-export function CloseModalButton({ onClose }: CloseModalButtonProps) {
+export function CloseModalButton({ onClose, className }: CloseModalButtonProps) {
   return (
     <button 
       className="font-bold hover:text-gray-300 transition-colors"
